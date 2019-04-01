a8fe66a0ded183f82ab4c641a2891968114a5b00
Ludovic <ludovic@hirlimann.net>
2019-04-01 16:34:38 +0200

    have something that actually works

--- log_lag.py
+++ log_lag.py
@@ -2,39 +2,24 @@
 import os
 import sys
 import time
-
+from datetime import datetime
+from functools import partial
 DEFAULT_LOG = "/usr/local/bro/logs/current/conn.log"
-
-def config():
-    print """
-graph_category network
-
-graph_title Bro log lag
-graph_vlabel lag
-graph_args --base 1000 --vertical-label seconds --lower-limit 0
-graph_info The bro log lag
-
-lag.label lag
-lag.info log message lag in seconds
-lag.min 0
-lag.warning :5000
-lag.warning 0:15
-lag.critical 0:60
-""".strip()
-
-    return 0
 
 def get_latest_time(fn):
     f = open(fn)
-
+    timestamps = []
     f.seek(-4096, os.SEEK_END)
     end = f.read().splitlines()[1:-1] #ignore possibly incomplete first and last lines
     times = [line.split()[0] for line in end]
-    timestamps = map(float, times)
+    for items in times:
+        k = time.strptime(items,"%Y-%m-%dT%H:%M:%SZ")
+        timestamps.append(time.mktime(k))
+
     latest = max(timestamps)
     return latest
 
-def lag(fn):
+def lag(fn, lagvalue):
     try :
         latest = get_latest_time(fn)
     except (IOError, ValueError):
@@ -43,13 +28,20 @@ def lag(fn):
         latest = get_latest_time(fn)
     now = time.time()
     lag = now - latest
-    print "lag.value %f" % lag
+    if (lag < lagvalue):
+        print "ERROR - File % lags by %f",fn,lag
+        sys.exit(2)
+    else:
+        print "OK - No lag for file %s",fn
+        sys.exit(0)
 
 if __name__ == "__main__":
 
-    filename = os.getenv("BRO_LAG_FILENAME", DEFAULT_LOG)
+#    filename = os.getenv("BRO_LAG_FILENAME", DEFAULT_LOG)
 
-    if sys.argv[1:] and sys.argv[1] == 'config':
-        config()
-    else:
-        lag(filename)
+#    if sys.argv[1:] and sys.argv[1] == 'config':
+#        config()
+#    else:
+#        lag(filename,lag)
+     print("file %s , lag %f",sys.argv[1],sys.argv[2])
+     lag(sys.argv[1],sys.argv[2])
