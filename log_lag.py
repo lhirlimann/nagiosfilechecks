27c7a973f2d72d413bcbd1254fcd6950ff906393
Ludovic <ludovic@hirlimann.net>
2019-04-02 09:41:48 +0200

    python 3 support and fixes for BRO/Zeek logs

--- log_lag.py
+++ log_lag.py
@@ -4,18 +4,14 @@ import sys
 import time
 from datetime import datetime
 from functools import partial
-DEFAULT_LOG = "/usr/local/bro/logs/current/conn.log"
 
 def get_latest_time(fn):
     f = open(fn)
-    timestamps = []
-    f.seek(-4096, os.SEEK_END)
+    f.seek(0, os.SEEK_END)
+    f.seek(f.tell() - 4096, os.SEEK_SET)
     end = f.read().splitlines()[1:-1] #ignore possibly incomplete first and last lines
     times = [line.split()[0] for line in end]
-    for items in times:
-        k = time.strptime(items,"%Y-%m-%dT%H:%M:%SZ")
-        timestamps.append(time.mktime(k))
-
+    timestamps = map(float, times)
     latest = max(timestamps)
     return latest
 
@@ -28,20 +24,13 @@ def lag(fn, lagvalue):
         latest = get_latest_time(fn)
     now = time.time()
     lag = now - latest
-    if (lag < lagvalue):
-        print "ERROR - File % lags by %f",fn,lag
+    if (lag < float(lagvalue)):
+        print ("ERROR - File %s lags by %f",fn,lag)
         sys.exit(2)
     else:
-        print "OK - No lag for file %s",fn
+        print ("OK - No lag for file %s",fn)
         sys.exit(0)
 
 if __name__ == "__main__":
 
-#    filename = os.getenv("BRO_LAG_FILENAME", DEFAULT_LOG)
-
-#    if sys.argv[1:] and sys.argv[1] == 'config':
-#        config()
-#    else:
-#        lag(filename,lag)
-     print("file %s , lag %f",sys.argv[1],sys.argv[2])
      lag(sys.argv[1],sys.argv[2])
